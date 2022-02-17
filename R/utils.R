e2fec5be45c6223b4e10f4eaea64cb8fabf686ac
Marco Zanotti <marco.zanotti@blogmeter.it>
2022-02-17 14:26:57 +0100

    update

--- R/utils.R
+++ R/utils.R
@@ -105,7 +105,7 @@ install_and_load <- function(pkgs, repos = getOption("repos")) {
 
 # Functions for rcod_lecture7_salesdash08_forecast
 aggregate_time_series <- function(data, time_unit = "month") {
-    
+
   output_tbl <- data %>%
     mutate(date = floor_date(order.date, unit = time_unit)) %>%
     group_by(date) %>%
@@ -113,13 +113,13 @@ aggregate_time_series <- function(data, time_unit = "month") {
     ungroup() %>%
     mutate(label_text = str_glue("Date: {date}
                                  Revenue: {scales::dollar(total_sales)}"))
-  
+
   return(output_tbl)
-    
+
 }
 
 plot_time_series <- function(data) {
-    
+
   g <- data %>%
     ggplot(aes(date, total_sales)) +
     geom_line(color = "#2c3e50") +
@@ -128,16 +128,16 @@ plot_time_series <- function(data) {
     expand_limits(y = 0) +
     scale_y_continuous(labels = scales::dollar_format()) +
     labs(x = "", y = "")
-  
+
   ggplotly(g, tooltip = "text")
-    
+
 }
 
 generate_forecast <- function(data, n_future = 12, seed = NULL) {
-    
+
   train_tbl <- data %>%
     tk_augment_timeseries_signature()
-  
+
   future_data_tbl <- data %>%
     tk_index() %>%
     tk_make_future_timeseries(
@@ -146,17 +146,17 @@ generate_forecast <- function(data, n_future = 12, seed = NULL) {
       inspect_months = TRUE
     ) %>%
     tk_get_timeseries_signature()
-  
+
   time_scale <- data %>%
     tk_index() %>%
     tk_get_timeseries_summary() %>%
     pull(scale)
-  
+
   if (time_scale == "year") {
     model <- linear_reg(mode = "regression") %>%
       set_engine(engine = "lm") %>%
       fit.model_spec(
-        total_sales ~ ., 
+        total_sales ~ .,
         data = train_tbl %>% select(total_sales, index.num)
       )
   } else {
@@ -172,11 +172,11 @@ generate_forecast <- function(data, n_future = 12, seed = NULL) {
     ) %>%
       set_engine(engine = "xgboost") %>%
       fit.model_spec(
-        total_sales ~ ., 
+        total_sales ~ .,
         data = train_tbl %>% select(-date, -label_text, -diff)
       )
   }
-  
+
   prediction_tbl <- predict(model, new_data = future_data_tbl) %>%
     bind_cols(future_data_tbl) %>%
     select(.pred, index) %>%
@@ -184,35 +184,35 @@ generate_forecast <- function(data, n_future = 12, seed = NULL) {
     mutate(label_text = str_glue("Date: {date}
                                  Revenue: {scales::dollar(total_sales)}")) %>%
     add_column(key = "Prediction")
-  
+
   output_tbl <- data %>%
     add_column(key = "Actual") %>%
     bind_rows(prediction_tbl)
-  
+
   return(output_tbl)
 
 }
 
 plot_forecast <- function(data) {
-    
+
   # Yearly - LM Smoother
   time_scale <- data %>%
     tk_index() %>%
     tk_get_timeseries_summary() %>%
     pull(scale)
-  
+
   # Only 1 Prediction - points
   n_predictions <- data %>%
     filter(key == "Prediction") %>%
     nrow()
-  
+
   g <- data %>%
     ggplot(aes(date, total_sales, color = key)) +
     geom_line() +
     scale_y_continuous(labels = scales::dollar_format()) +
     expand_limits(y = 0) +
     labs(x = "", y = "")
-  
+
   # Yearly - LM Smoother
   if (time_scale == "year") {
     g <- g +
@@ -220,14 +220,14 @@ plot_forecast <- function(data) {
   } else {
     g <- g + geom_smooth(method = "loess", span = 0.2, size = 0.5)
   }
-  
+
   # Only 1 Prediction
   if (n_predictions == 1) {
     g <- g + geom_point(aes(text = label_text), size = 1)
   } else {
     g <- g + geom_point(aes(text = label_text), size = 0.01)
   }
-  
+
   ggplotly(g, tooltip = "text")
-  
+
 }
